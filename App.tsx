9a07991f6ff4bc893fa65c19ca84750a095f0d1a
nagayamat <tomoya.nagayama@daikin.co.jp>
2025-06-12 10:38:42 +0900

    test

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ function sayHello(name:String ) { // 型の間違いとスペースの問題
 console.log(`${greeting}, ${name}!`); // インデントが不正
 }
 
+
 const users = ["Alice", "Bob", "Charlie"]; // 配列の記述方法
 users.ForEach(user=> {sayHello(user)}); // 大文字の間違い
 
