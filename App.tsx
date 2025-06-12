5ad1a4e9e217384048f747d857ccbaf19042d7b5
nagayamat <tomoya.nagayama@daikin.co.jp>
2025-06-12 10:25:07 +0900

    Linter/Formatter Test用TypeScript First Commit

---
+++ App.tsx
@@ -0,0 +1,9 @@
+const greeting:string= "Hello, world"; // 型注釈のスペースがない
+function sayHello(name:String ) { // 型の間違いとスペースの問題
+console.log(`${greeting}, ${name}!`); // インデントが不正
+}
+
+const users = ["Alice", "Bob", "Charlie"]; // 配列の記述方法
+users.ForEach(user=> {sayHello(user)}); // 大文字の間違い
+
+sayHello("Dan");
